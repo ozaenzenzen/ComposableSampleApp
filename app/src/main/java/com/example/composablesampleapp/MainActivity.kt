daac7ea53e01eb7e916fee8eeefdb91e5d20752d
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-06-29 21:09:22 +0700

    update: implement library

--- app/src/main/java/com/example/composablesampleapp/MainActivity.kt
+++ app/src/main/java/com/example/composablesampleapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.composablesampleapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MotionEvent
 import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +21,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -30,9 +37,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,10 +62,22 @@ class MainActivity : ComponentActivity() {
             secret = "1dc8e065-2915-4b4e-8df2-45040e8314bd",
             flavorData = "staging",
         )
+
+
         setContent {
+            val contextUpper = LocalContext.current
+
+            // Initial position state
+            val offsetX = remember { mutableStateOf(0f) }
+            val offsetY = remember { mutableStateOf(0f) }
+
             ComposableSampleAppTheme {
                 Box(
                     modifier = Modifier.fillMaxSize()
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(16.dp), // padding to keep button from touching edges
+//                    contentAlignment = Alignment.BottomEnd
                 ) {
                     Scaffold(
                         topBar = {
@@ -86,7 +107,21 @@ class MainActivity : ComponentActivity() {
                             object : FrameLayout(context) {
                                 init {
                                     val floatingButton = KonnekNative.getFloatingButton(context)
-                                    addView(floatingButton)
+
+                                    // Set layout parameters as if you were in XML
+                                    val layoutParams = FrameLayout.LayoutParams(
+                                        FrameLayout.LayoutParams.WRAP_CONTENT,
+                                        FrameLayout.LayoutParams.WRAP_CONTENT
+                                    ).apply {
+                                        gravity = Gravity.BOTTOM or Gravity.END
+                                        marginEnd = 10.toPx(contextUpper)
+                                        bottomMargin = 10.toPx(contextUpper)
+                                    }
+
+                                    floatingButton.layoutParams = layoutParams
+
+                                    // Return the view to AndroidView
+                                     addView(floatingButton)
                                 }
 
                                 override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
@@ -101,6 +136,7 @@ class MainActivity : ComponentActivity() {
                             }
                         },
                         modifier = Modifier
+//                            .background(Color(0xff000000))
                             .fillMaxSize()
                             .align(Alignment.BottomEnd)
                             .padding(16.dp)
@@ -111,6 +147,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+fun Int.toPx(context: Context): Int =
+    (this * context.resources.displayMetrics.density).toInt()
+
+private fun Dp.toPx(context: Context): Dp =
+    (this * context.resources.displayMetrics.density)
+
+
+//fun Int.toPx(context: Context): Int =
+//    (this * context.resources.displayMetrics.density).toInt()
 
 @Composable
 fun AdvancedDraggableButton() {
@@ -227,6 +273,11 @@ fun ColumnData(modifier: Modifier = Modifier) {
             Text(
                 text = "String 3"
             )
+            Button(onClick = {
+                println("test")
+            }) {
+                Text("Filled")
+            }
         }
     )
 }
